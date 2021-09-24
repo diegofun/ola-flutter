5088dcbd57676837369940eb8d3231ecee8768bb
diegofun <diegofun3@gmail.com>
2021-09-23 20:43:26 -0500

    algo sobre el home

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Ola',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -47,6 +47,14 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  bool logged = false;
+  int _currentIndex=0;
+  //List _screens=[HomePage(),SearchPage(),CategoryPage(),AccountPage()];
+
+  /*void _updateIndex(int value) {
+    setState(() {
+      _currentIndex = value;
+    });*/
 
   void _incrementCounter() {
     setState(() {
@@ -68,46 +76,100 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          //currentIndex: _currentIndex,
+          //onTap: _updateIndex,
+          selectedItemColor: Colors.red,
+          selectedFontSize: 13,
+          unselectedFontSize: 13,
+          iconSize: 30,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Inicio',
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'Perfil',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite),
+              label: 'Favoritos',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.monetization_on),
+              label: 'Vender',
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Container(
+        margin: new EdgeInsets.all(15.0),
+    child: new SingleChildScrollView(
+    child: Column(
+    // Column is also a layout widget. It takes a list of children and
+    // arranges them vertically. By default, it sizes itself to fit its
+    // children horizontally, and tries to be as tall as its parent.
+    //
+    // Invoke "debug painting" (press "p" in the console, choose the
+    // "Toggle Debug Paint" action from the Flutter Inspector in Android
+    // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+    // to see the wireframe for each widget.
+    //
+    // Column has various properties to control how it sizes itself and
+    // how it positions its children. Here we use mainAxisAlignment to
+    // center the children vertically; the main axis here is the vertical
+    // axis because Columns are vertical (the cross axis would be
+    // horizontal).
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: <Widget>[
+    Text(
+    'You have pushed the button this many times:',
+    ),
+    Text(
+    '$_counter',
+    style: Theme.of(context).textTheme.headline4,
+    ),
+    TextFormField(
+    cursorColor: Colors.red,
+    keyboardType: TextInputType.emailAddress,
+    decoration: new InputDecoration(
+    border: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.red, width: 5.0),
+    ),
+    contentPadding: EdgeInsets.only(
+    left: 15, bottom: 11, top: 11, right: 15),
+    hintText: "Email"),
+    ),
+    SizedBox(
+    height: 15,
+    ),
+    TextFormField(
+    cursorColor: Colors.red,
+    obscureText: true,
+    keyboardType: TextInputType.visiblePassword,
+    decoration: new InputDecoration(
+    border: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.red, width: 5.0),
+    ),
+    contentPadding: EdgeInsets.only(
+    left: 15, bottom: 11, top: 11, right: 15),
+    hintText: "Contraseña"),
+    ),
+    ],
+    ),
+    ),
+    ),
+    ),
+    floatingActionButton: FloatingActionButton(
+    onPressed: _incrementCounter,
+    tooltip: 'Increment',
+    child: Icon(Icons.add),
+    )
+    , // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
