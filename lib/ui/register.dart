9acb6817bd3d57162565bb945798aacc1aee1a6b
diegofun <diegofun3@gmail.com>
2021-10-01 15:47:18 -0500

    working in everything

--- lib/ui/register.dart
+++ lib/ui/register.dart
@@ -1,5 +1,8 @@
+import 'package:image_picker/image_picker.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'dart:io';
 
 class Register extends StatefulWidget{
   Register({Key? key, required this.title}) : super(key: key);
@@ -13,6 +16,13 @@ class Register extends StatefulWidget{
 class _RegisterState extends State<Register> {
   _RegisterState(this.title);
   final String title;
+  final ImagePicker _picker = ImagePicker();
+
+  Future<Image> bgImage() async {
+    final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+    return Image.file(File(image!.path));
+  }
+
 
   @override
   initState() {
@@ -39,6 +49,7 @@ class _RegisterState extends State<Register> {
               child: Column(
                 children: <Widget>[
                   Text(title),
+                  //Image.asset('assets/images/lion.png', width: 50, height: 50),
                   TextFormField(
                     cursorColor: Colors.red,
                     keyboardType: TextInputType.emailAddress,
